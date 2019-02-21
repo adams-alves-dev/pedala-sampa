f0d9cff6136cb8e637d2b58c68405348909d3d56
Adams Alves <alves.adams@gmail.com>
2019-02-21 19:14:41 -0300

    add _id_ in data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
-    <div v-for="Club in Clubs" :key="Club.id">
-      <article class="card">
-        <div class="content">
-          <h3><a :href="Club.fields.link">{{ Club.fields.name }}</a> - <span>{{ Club.fields.effort }}</span></h3>
-          <p>Local de concentração: <strong>{{ Club.fields.departure_location }}</strong> - {{ Club.fields.day }}, {{ Club.fields.start_hour }}</p>
-        </div>
-      </article>
-    </div>
+    <article class="card" v-for="Club in Clubs" :key="Club.id" :data-id="Club.id">
+      <div class="content">
+        <h3><a :href="Club.fields.link">{{ Club.fields.name }}</a> - <span>{{ Club.fields.effort }}</span></h3>
+        <p>Local de concentração: <strong>{{ Club.fields.departure_location }}</strong> - {{ Club.fields.day }}, {{ Club.fields.start_hour }}</p>
+      </div>
+    </article>
   </div>
 </template>
 
