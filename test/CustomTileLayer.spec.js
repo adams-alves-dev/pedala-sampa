1d4ec4c339ba58d73dc222f03ab55f93865501c3
Adams Alves <alves.adams@gmail.com>
2021-09-23 14:32:55 -0300

    Add CustomTileLayer tests

---
+++ test/CustomTileLayer.spec.js
@@ -0,0 +1,76 @@
+import { mount } from '@vue/test-utils'
+import CustomTileLayer from '@/components/Map/CustomTileLayer.vue'
+
+describe('CustomTileLayer', () => {
+  describe('Without $config variables', () => {
+    let wrapper
+
+    beforeAll(() => {
+      wrapper = mount(CustomTileLayer, {
+        mocks: {
+          $config: {},
+        },
+        stubs: ['l-tile-layer'],
+      })
+    })
+
+    test('is a Vue instance', () => {
+      expect(wrapper).toBeTruthy()
+    })
+
+    test('is a component named `CustomTileLayer`', () => {
+      expect(wrapper.findComponent({ name: 'CustomTileLayer' }).exists()).toBe(
+        true
+      )
+    })
+
+    test('is in component `l-tile-layer` have a attribute `attribution`', () => {
+      expect(wrapper.find('attribution').exists())
+    })
+
+    test('is in component `l-tile-layer` have a attribute `attribution` with content', () => {
+      expect(wrapper.attributes().attribution).toBe(
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      )
+    })
+
+    test('is in component `l-tile-layer` have a attribute `url`', () => {
+      expect(wrapper.find('url').exists())
+    })
+
+    test('is in component `l-tile-layer` have a attribute `url` with content', () => {
+      expect(wrapper.attributes().url).toBe(
+        'https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      )
+    })
+  })
+
+  describe('With $config variables', () => {
+    let wrapper
+
+    beforeAll(() => {
+      wrapper = mount(CustomTileLayer, {
+        mocks: {
+          $config: {
+            MAPBOX_USERID: 'adamsalves',
+            MAPBOX_STYLEID: 'cktt15ncyIsfqthtmd',
+            MAPBOX_API_KEY: 'eyJ1IjoiYWRhbXNhbHZlcyIsImE',
+          },
+        },
+        stubs: ['l-tile-layer'],
+      })
+    })
+
+    test('is in component `l-tile-layer` have a attribute `attribution` with content', () => {
+      expect(wrapper.attributes().attribution).toBe(
+        '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      )
+    })
+
+    test('is in component `l-tile-layer` have a attribute `url` with content', () => {
+      expect(wrapper.attributes().url).toBe(
+        `https://api.mapbox.com/styles/v1/adamsalves/cktt15ncyIsfqthtmd/tiles/256/{z}/{x}/{y}@2x?access_token=eyJ1IjoiYWRhbXNhbHZlcyIsImE`
+      )
+    })
+  })
+})
